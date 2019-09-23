d6f978dd04dfa5217e267d83eb1c55cab6dd03a0
Marcial Mendoza <MendozaM0717@gmail.com>
2019-09-23 16:54:34 -0700

    Added bluetooth settings

--- app/src/main/java/com/example/assurex/Speed.java
+++ app/src/main/java/com/example/assurex/Speed.java
@@ -2,20 +2,24 @@ package com.example.assurex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
 public class Speed extends AppCompatActivity {
 
     private Button testing;
+    BluetoothAdapter myBtAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed);
 
+        myBtAdapter = BluetoothAdapter.getDefaultAdapter();
         testing = findViewById(R.id.testing);
         testing.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +28,10 @@ public class Speed extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void BtSettingsClicked(View view) {
+        Intent openBtSettings = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+        startActivity(openBtSettings);
     }
 }
