71e9bd867e34a9e578d5457d82006fc80fdd246f
arcturusthethird <52969170+arcturusthethird@users.noreply.github.com>
2019-10-01 18:53:57 -0700

    switched to SQLite using ROOM

--- app/src/main/java/com/example/assurex/model/DataItem.java
+++ app/src/main/java/com/example/assurex/model/DataItem.java
@@ -4,42 +4,44 @@ import android.content.ContentValues;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.example.assurex.database.ItemsTable;
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 import java.util.Calendar;
 import java.util.UUID;
 
+@Entity
 public class DataItem implements Parcelable {
-    
-    //tripID
-    //date
-    //timeStamp
-    //avg speed
-    //top speed
-    //top acceleration rate
-    //avg acceleration rate
-    //top deceleration rate
-    //avg deceleration rate
 
+    //@PrimaryKey(autoGenerate = true)
+    @PrimaryKey
+    @NonNull
     private String tripId;
+    @ColumnInfo
     private String date;
+    @ColumnInfo
     private String timeStamp;
+    @ColumnInfo
     private double speed;
-    private double accelRate;
+    @ColumnInfo
+    private double accelerationRate;
 
     public DataItem() {
     }
 
     //public DataItem(String tripId, String date, String timeStamp, double topSpeed) {
-    public DataItem(String tripId, String date, String timeStamp, double speed, double accelRate) {
-
+    @Ignore
+    public DataItem(String tripId, String date, String timeStamp, double speed, double accelerationRate) {
 
         if (tripId == null) {
             tripId = UUID.randomUUID().toString();
             /*
             * todo set tripID to a value that is the same for every entry for the same trip
             *  but changes to a new value for another trip. Perhaps set to change tripId if
-            *   obd2 connection is termimnated and assume vehicle has turned off
+            *   obd2 connection is terminated and assume vehicle has turned off
             * */
         }
 
@@ -49,61 +51,28 @@ public class DataItem implements Parcelable {
         this.date = date;
         this.timeStamp = timeStamp;
         this.speed = speed;
-        this.accelRate = accelRate;
+        this.accelerationRate = accelerationRate;
     }
 
-    public String getTripId() {
-        return tripId;
-    }
+    public String getTripId() { return tripId; }
 
-    public void setTripId(String tripId) {
-        this.tripId = tripId;
-    }
+    public void setTripId(String tripId) { this.tripId = tripId; }
 
-    public String getDate() {
-        return date;
-    }
+    public String getDate() { return date; }
 
-    //public void setDate(String Date) {
-    //    this.date = Date;
-    //}
+    public void setDate(String date) { this.date = date; }
 
-    public String getTimeStamp() {
-        return timeStamp;
-    }
+    public String getTimeStamp() { return timeStamp; }
 
-    //public void setTimeStamp(String timeStamp) {
-    //    this.timeStamp = timeStamp;
-    //}
+    public void setTimeStamp(String timeStamp) { this.timeStamp = timeStamp; }
 
+    public double getSpeed() { return speed; }
 
-    public double getSpeed() {
-        return speed;
-    }
+    public void setSpeed(double speed) { this.speed = speed; }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
+    public double getAccelerationRate() { return accelerationRate; }
 
-    public double getAccelRate() {
-        return accelRate;
-    }
-
-    public void setAccelRate(double accelRate) {
-        this.accelRate = accelRate;
-    }
-
-    public ContentValues toValues(){
-        ContentValues values = new ContentValues(5); //7 columns, so passed value 7
-
-        values.put(ItemsTable.COLUMN_TRIP_ID, tripId);
-        values.put(ItemsTable.COLUMN_DATE, date);
-        values.put(ItemsTable.COLUMN_TIMESTAMP, timeStamp);
-        values.put(ItemsTable.COLUMN_SPEED, speed);
-        values.put(ItemsTable.COLUMN_ACCELRATE, accelRate);
-        return values;
-
-    }
+    public void setAccelerationRate(double accelerationRate) { this.accelerationRate = accelerationRate; }
 
     @Override
     public String toString() {
@@ -112,14 +81,12 @@ public class DataItem implements Parcelable {
                 ", date='" + date + '\'' +
                 ", timeStamp='" + timeStamp + '\'' +
                 ", Speed='" + speed + '\'' +
-                ", accelRate='" + accelRate +
+                ", accelRate='" + accelerationRate +
                 '}';
     }
 
     @Override
-    public int describeContents() {
-        return 0;
-    }
+    public int describeContents() { return 0; }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
@@ -127,7 +94,7 @@ public class DataItem implements Parcelable {
         dest.writeString(this.date);
         dest.writeString(this.timeStamp);
         dest.writeDouble(this.speed);
-        dest.writeDouble(this.accelRate);
+        dest.writeDouble(this.accelerationRate);
     }
 
     protected DataItem(Parcel in) {
@@ -135,7 +102,7 @@ public class DataItem implements Parcelable {
         this.date = in.readString();
         this.timeStamp = in.readString();
         this.speed = in.readDouble();
-        this.accelRate = in.readDouble();
+        this.accelerationRate = in.readDouble();
     }
 
     public static final Parcelable.Creator<DataItem> CREATOR = new Parcelable.Creator<DataItem>() {
