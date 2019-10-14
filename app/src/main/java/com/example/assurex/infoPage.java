d454db9d38204b427de3d39b5ab700f5b23b6424
Clientings <ben.stanke@me.com>
2019-10-14 08:55:09 -0700

    fetched 10-14-19

--- app/src/main/java/com/example/assurex/infoPage.java
+++ app/src/main/java/com/example/assurex/infoPage.java
@@ -9,11 +9,17 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class infoPage extends AppCompatActivity {
+public class infoPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     private boolean avSpButTrue = false;
@@ -28,20 +34,50 @@ public class infoPage extends AppCompatActivity {
     private boolean insButTrue = false;
     private Button insBut;
     private LinearLayout insButWindow;
+    private boolean calShowing = false;
+    private LinearLayout calenderWindow;
+    private boolean spinShowing = false;
+    private LinearLayout spinWindow;
+    private Spinner tripSpinner;
+    private ArrayAdapter<CharSequence> adapter;
+    private boolean locShowing = false;
+    private LinearLayout locWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
 
+        //for average speed button
         avSpBut = findViewById(R.id.avSpBut);
         avSpButWindow = findViewById(R.id.avSpWindow);
+
+        //for acceleration button
         accBut = findViewById(R.id.accBut);
         accButWindow = findViewById(R.id.accWindow);
+
+        //for engine information
         engInfBut = findViewById(R.id.engInfBut);
         engInfButWindow = findViewById(R.id.engInfWindow);
+
+        //for insurance button
         insBut = findViewById(R.id.insBut);
         insButWindow = findViewById(R.id.insWindow);
+
+        //for calender drop down
+        calenderWindow = findViewById(R.id.calender);
+
+        //for spinner drop down
+        spinWindow = findViewById(R.id.spinnerWindow);
+        tripSpinner = findViewById(R.id.tripSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tripSpinner.setAdapter(adapter);
+        //tripSpinner.setOnItemClickListener(this::onItemSelected);
+
+        //for calender drop down
+        locWindow = findViewById(R.id.locationWindow);
+
 
 
     }//end onCreate
@@ -101,4 +137,75 @@ public class infoPage extends AppCompatActivity {
             insButTrue = false;
         }//end else
     }
+
+    public void dateClicked(View view) {
+        if(calShowing==false){
+            calenderWindow.setVisibility(View.VISIBLE);
+            calShowing = true;
+            if(spinShowing==true){
+                spinWindow.setVisibility(View.GONE);
+                spinShowing = false;
+            }//end if
+            if(locShowing==true){
+                locWindow.setVisibility(View.GONE);
+                locShowing = false;
+            }//end ifb
+        }//end if
+        else{
+            calenderWindow.setVisibility(View.GONE);
+            calShowing = false;
+        }//end else
+    }
+
+    public void tripClicked(View view) {
+        if(spinShowing==false){
+            spinWindow.setVisibility(View.VISIBLE);
+            spinShowing = true;
+            if(calShowing==true){
+                calenderWindow.setVisibility(View.GONE);
+                calShowing = false;
+            }//end if
+            if(locShowing==true){
+                locWindow.setVisibility(View.GONE);
+                locShowing = false;
+            }//end if
+        }//end if
+        else{
+            spinWindow.setVisibility(View.GONE);
+            spinShowing = false;
+        }//end else
+    }
+
+    //for spinner -----
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    //end for spinner ------
+
+    public void locationClicked(View view) {
+        if(locShowing==false){
+            locWindow.setVisibility(View.VISIBLE);
+            if(calShowing==true){
+                calenderWindow.setVisibility(View.GONE);
+                calShowing = false;
+            }//end if
+            if(spinShowing==true){
+                spinWindow.setVisibility(View.GONE);
+                spinShowing = false;
+            }//end if
+            locShowing = true;
+        }//end if
+        else{
+            locWindow.setVisibility(View.GONE);
+            locShowing = false;
+        }//end else
+    }
+
 }
