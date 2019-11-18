3e96b1e06759e021b7e2e9506d159d9ec21af4b5
Andrew Alarcon <andrewan47@gmail.com>
2019-11-18 09:37:34 -0800

    user database

--- app/src/main/java/com/example/assurex/Register.java
+++ app/src/main/java/com/example/assurex/Register.java
@@ -1,8 +1,10 @@
 package com.example.assurex;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,15 @@ import com.example.assurex.database.AppDatabase;
 
 import org.greenrobot.eventbus.EventBus;
 public class Register extends AppCompatActivity{
+    EditText userInput;
+    EditText passInput;
+    EditText secPass;
+    EditText emailInput;
+    String newUser;
+    String newEmail;
+    String newPass;
+    String rePass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +33,74 @@ public class Register extends AppCompatActivity{
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        emailInput = findViewById(R.id.newEmailText);
+        passInput = findViewById(R.id.newPassText);
+        secPass = findViewById(R.id.rePassText);
+        userInput = findViewById(R.id.newUserText);
     }
+
     @Override
     protected void onDestroy() {
         AppDatabase.destroyInstance();
         EventBus.getDefault().unregister(this);
         super.onDestroy();
     }
+
     public void RegisterClicked(View view) {
-        startActivity(new Intent(getApplicationContext(), Speed.class));
-        finish();
+        newEmail = emailInput.getText().toString();
+        newPass = passInput.getText().toString();
+        rePass = secPass.getText().toString();
+        newUser = userInput.getText().toString();
+
+        boolean userPass = userCheck(newUser);
+        boolean emailPass = emailCheck(newEmail);
+        boolean passwordPass = passCheck(newPass, rePass);
+
+        if(userPass && emailPass && passwordPass) {
+            UserRepository register = new UserRepository(getApplicationContext());
+            register.insertUser(newUser, newPass, newEmail);
+
+            startActivity(new Intent(getApplicationContext(), Speed.class));
+            //finish();
+        }
+    }
+
+    public boolean emailCheck(String email) {
+        if(email.isEmpty()) {
+            emailInput.setError("Please enter an Email address");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailInput.setError("Invalid Email address");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean userCheck(String user) {
+        if(user.isEmpty()) {
+            userInput.setError("Please enter a username");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean passCheck(String pass, String sec) {
+        if(pass.isEmpty()) {
+            passInput.setError("Please enter a password");
+            return false;
+        }
+        else if(!pass.equals(sec)) {
+            secPass.setError("Passwords do not match");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
