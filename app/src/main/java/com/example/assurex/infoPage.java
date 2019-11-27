305e3280c52c1228957a529370cc085d6df672db
arcturusthethird <52969170+arcturusthethird@users.noreply.github.com>
2019-11-27 12:11:44 -0800

    Misc
    changes to some formats in raw data collection entries. changes and bug fixes for info page.

--- app/src/main/java/com/example/assurex/infoPage.java
+++ app/src/main/java/com/example/assurex/infoPage.java
@@ -204,6 +204,8 @@ public class infoPage extends AppCompatActivity implements AdapterView.OnItemSel
         SimpleDateFormat mdformat = new SimpleDateFormat("MM - dd - yyyy ");
         date = mdformat.format(calendar.getTime());
         calText.setText(date);
+        dbQueryDate = date.replaceAll("\\s+", "");
+        addItemsOnSpinner();
 
         calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -437,7 +439,7 @@ public class infoPage extends AppCompatActivity implements AdapterView.OnItemSel
     }
 
     //end for spinner ------
-
+    /*
     //for location -------
     public void locationClicked(View view) {
         //location
@@ -531,6 +533,21 @@ public class infoPage extends AppCompatActivity implements AdapterView.OnItemSel
     }//end onpermissionresult
 
     //end for location ------
+    */
+
+    public void currentClicked(View view){
+
+        if (tTopSpeed > 0 || tTopAcceleration > 0) {
+            topSpeed.setText("Top Speed: "+tTopSpeed+ " MPH");
+            topAcc.setText("Top Acceleration: "+tTopAcceleration + " MPH/S");
+            if (tEngineTroubleCodes ==  null){
+                engStatus.setText("Status: No outstanding problems");
+            }
+            else engStatus.setText("Status: "+tEngineTroubleCodes);
+        } else {
+            Toast.makeText(this, "Not Currently On Trip", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     class RawDataReceiver extends BroadcastReceiver {
 
@@ -567,10 +584,10 @@ public class infoPage extends AppCompatActivity implements AdapterView.OnItemSel
         dateIns.setText(date);
 
         //set Av Speed
-        topSpeed.setText("Top Speed: "+displayedTopSpeed+ " mph");
+        topSpeed.setText("Top Speed: "+displayedTopSpeed+ " MPH");
 
         //set Acceleration
-        topAcc.setText("Top Acceleration: "+displayedTopAcceleration);
+        topAcc.setText("Top Acceleration: "+displayedTopAcceleration + " MPH/S");
 
         //set Eng Inf
         if (displayedEngineTroubleCodes ==  null){
@@ -582,7 +599,7 @@ public class infoPage extends AppCompatActivity implements AdapterView.OnItemSel
         if (tempTripSummary ==  null){
             tripSum.setText("Everything is looking good");
         }
-        else tripSum.setText((CharSequence) tempTripSummary);
+        else tripSum.setText("No Trip Summary Selected");
 
     }//end update Data
 }
