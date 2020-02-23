d1c6832fee9a84369fddbddcb7d64338dd340096
Marcial Mendoza <MendozaM0717@gmail.com>
2020-02-22 20:54:49 -0800

    registration now requires registration of device

--- app/src/main/java/com/example/assurex/Register.java
+++ app/src/main/java/com/example/assurex/Register.java
@@ -1,27 +1,40 @@
 package com.example.assurex;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.assurex.database.AppDatabase;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.io.IOException;
+import java.util.Set;
+
 public class Register extends AppCompatActivity{
     EditText userInput;
     EditText passInput;
     EditText secPass;
     EditText emailInput;
+    Button regDevice;
     String newUser;
     String newEmail;
     String newPass;
     String rePass;
+
+    String deviceAddress = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +64,7 @@ public class Register extends AppCompatActivity{
         passInput = findViewById(R.id.newPassText);
         secPass = findViewById(R.id.rePassText);
         userInput = findViewById(R.id.newUserText);
+        regDevice = findViewById(R.id.deviceReg);
     }
 
     @Override
@@ -69,13 +83,29 @@ public class Register extends AppCompatActivity{
         boolean userPass = userCheck(newUser);
         boolean emailPass = emailCheck(newEmail);
         boolean passwordPass = passCheck(newPass, rePass);
+        boolean devicePass = deviceCheck();
 
-        if(userPass && emailPass && passwordPass) {
+
+        if(userPass && emailPass && passwordPass && devicePass) {
             UserRepository register = new UserRepository(getApplicationContext());
+            // todo: insert deviceAddress along with other info
             register.insertUser(newUser, newPass, newEmail);
 
             startActivity(new Intent(getApplicationContext(), Speed.class));
             //finish();
+        }
+    }
+
+    public boolean deviceCheck()
+    {
+        if (deviceAddress == "")
+        {
+            regDevice.setError("");
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
@@ -115,5 +145,46 @@ public class Register extends AppCompatActivity{
         else {
             return true;
         }
+    }
+
+    public void devRegClicked(View view) {
+        BluetoothAdapter myBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice myBtDevice = null;
+        Set<BluetoothDevice> pairedDevices;
+        deviceAddress = "";
+
+        // look in phone's bonded devices
+        for(int i = 0; i < 2; i++) {
+            pairedDevices = myBtAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    if (device.getName().equals("OBDII")) {
+                        Toast.makeText(this, "found device", Toast.LENGTH_SHORT).show();
+                        myBtDevice = device;
+                        deviceAddress = myBtDevice.getAddress();
+                        regDevice.setError(null);
+                        regDevice.setText("Device Found");
+                        regDevice.setBackgroundColor(Color.parseColor("green"));
+                    }
+                }
+                if (myBtDevice == null) {
+                    Intent openBtSettings = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                    openBtSettings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(openBtSettings);
+                }
+            } else {
+                Intent openBtSettings = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+                openBtSettings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(openBtSettings);
+            }
+            if (myBtDevice != null)
+            {
+                break;
+            }
+        }
+
+
+
+
     }
 }
